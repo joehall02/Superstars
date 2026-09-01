682ef3b4f1efbbce8c880c4677455462016a2cc7
Joe Hall <joehall0207@gmail.com>
2026-09-01 19:37:29 +0100

    Move top level wrappers to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router';
 
+import { ConfigProvider } from './context/configProvider';
+import { queryClient } from './queryClient';
 import { router } from './router';
+import { ThemeModeProvider } from './theme/themeModeProvider';
 
-const App = () => <RouterProvider router={router} />;
+const App = () => (
+	<ThemeModeProvider>
+		<QueryClientProvider client={queryClient}>
+			<ConfigProvider>
+				<RouterProvider router={router} />
+			</ConfigProvider>
+		</QueryClientProvider>
+	</ThemeModeProvider>
+);
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,10 @@
-import { QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
-import { ConfigProvider } from './context/configProvider.tsx';
-import { queryClient } from './queryClient.ts';
-import { ThemeModeProvider } from './theme/themeModeProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<ThemeModeProvider>
-			<QueryClientProvider client={queryClient}>
-				<ConfigProvider>
-					<App />
-				</ConfigProvider>
-			</QueryClientProvider>
-		</ThemeModeProvider>
+		<App />
 	</StrictMode>,
 );
