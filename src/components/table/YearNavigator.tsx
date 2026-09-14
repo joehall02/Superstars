af2d44bd243626b78b7f2b93cde8b45a733ce98a
Joe Hall <joehall0207@gmail.com>
2026-09-14 22:47:17 +0100

    Lint

--- src/components/table/YearNavigator.tsx
+++ src/components/table/YearNavigator.tsx
@@ -23,11 +23,25 @@ export const YearNavigator = ({ year, onPrevious, onNext, canGoPrevious, canGoNe
 
 	return (
 		<Box className={classes.bar}>
-			<IconButton aria-label='Previous year' onClick={onPrevious} disabled={!canGoPrevious} size='small' color='inherit' className={classes.arrow}>
+			<IconButton
+				aria-label='Previous year'
+				onClick={onPrevious}
+				disabled={!canGoPrevious}
+				size='small'
+				color='inherit'
+				className={classes.arrow}
+			>
 				<ChevronLeftIcon />
 			</IconButton>
 			<Typography variant='subtitle1' className={classes.year}>{year}</Typography>
-			<IconButton aria-label='Next year' onClick={onNext} disabled={!canGoNext} size='small' color='inherit' className={classes.arrow}>
+			<IconButton
+				aria-label='Next year'
+				onClick={onNext}
+				disabled={!canGoNext}
+				size='small'
+				color='inherit'
+				className={classes.arrow}
+			>
 				<ChevronRightIcon />
 			</IconButton>
 		</Box>
